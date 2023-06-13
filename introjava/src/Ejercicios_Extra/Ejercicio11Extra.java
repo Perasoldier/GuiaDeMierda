a5f0648a7a96a18568d27a42c7a49b4b1581808d
Santiago Peralta <santiago.peralta93@gmail.com>
2023-06-12 21:33:55 -0300

    ejercicio 11 extra

---
+++ introjava/src/Ejercicios_Extra/Ejercicio11Extra.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_Extra;
+
+import java.util.Scanner;
+
+/**
+11. Escribir un programa que lea un número entero y devuelva el número de dígitos que
+componen ese número. Por ejemplo, si introducimos el número 12345, el programa
+deberá devolver 5. Calcular la cantidad de dígitos matemáticamente utilizando el
+operador de división. Nota: recordar que las variables de tipo entero truncan los
+números o resultados.
+ * @author peras
+ */
+public class Ejercicio11Extra {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int numero = scan.nextInt();
+        
+        System.out.println(numero);
+        
+        
+    }
+}
